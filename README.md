5cef1722e0607857188e2ace149e32d3474f8018
hchiam <hchiam@gmail.com>
2020-01-27 18:57:20 -0500

    customizing

--- README.md
+++ README.md
@@ -2,4 +2,20 @@
 
 Capture snippets of code inspiration while on the go.
 
+## Run locally
+
+```bash
+yarn
+```
+
+and then
+
+```bash
+yarn start
+```
+
+## Extra info
+
 You can generate a [dependency graph](https://github.com/hchiam/learning-dependency-cruiser) with `bash show_dep_graph.sh`.
+
+![Dependency graph.](https://github.com/hchiam/code-inspiration/blob/master/dependencygraph.svg)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Draggable from 'react-draggable';
 
 function App() {
+  const initialInput = '';
+  const [input, setInput] = React.useState(initialInput);
+  const [ideas, setIdeas] = React.useState([]);
+  const [displayOptionIndex, setDisplayOptionIndex] = React.useState(-1);
+  const focusTextArea = () => {
+    document.querySelector('textarea').focus();
+  };
+  const updateInput = () => {
+    const textarea = document.querySelector('textarea');
+    if (textarea.value === '') {
+      textarea.focus();
+      return;
+    }
+    setInput('');
+    setIdeas(ideas.concat(input));
+    textarea.focus();
+  };
+  const showOptions = (e, index) => {
+    setDisplayOptionIndex(index);
+    setTimeout(() => {
+      setDisplayOptionIndex(-1);
+    }, 1000);
+  };
+  const deleteIdea = (index) => {
+    setIdeas(ideas.filter((e, id) => id !== index));
+    setDisplayOptionIndex(-1);
+    focusTextArea();
+  };
+  const saveIdea = (index) => {
+    // TODO
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Capture <code>code</code> inspiration:</h1>
+        <div id="split-container">
+          <div>
+            <textarea onInput={(e) => setInput(e.target.value)}
+                      value={input}
+                      placeholder="type code here"
+                      autoFocus/>
+            <button onClick={updateInput}
+                    style={{display: input !== '' ? 'block' : 'none', margin: 'auto'}}>Add idea</button>
+          </div>
+          <pre className="react-markdown"
+              style={{display: input !== '' ? 'block' : 'none'}}>
+            <code class="language-js">{input}</code>
+          </pre>
+        </div>
+        <p style={{display: ideas.length > 0 ? 'block' : 'none'}}>_____________________</p>
+        <div id="ideas">
+          {
+            ideas.map((idea, index) =>
+              // (note: include just one element within Draggable)
+              <Draggable>
+                <pre className="react-markdown"
+                     key={index}
+                     onMouseEnter={(e) => showOptions(e, index)}
+                     title="Psst! You can drag me around the screen.">
+                  <button className="idea-button"
+                          style={{display: displayOptionIndex === index ? 'block' : 'none'}}
+                          onClick={() => deleteIdea(index)}>X</button>
+                  <code class="language-js">{idea}</code>
+                </pre>
+              </Draggable>
+            )
+          }
+        </div>
       </header>
     </div>
   );
