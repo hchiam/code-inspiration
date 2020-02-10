4077ec7a3f5dd3ff0a5793a64d494e7e81c4b71d
hchiam <hchiam@gmail.com>
2020-02-10 09:46:08 -0500

    http -> https redirect

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+// http -> https
+if (!String(window.location).startsWith('http://localhost') && window.location.protocol !== 'https:') {
+  window.location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+}
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
