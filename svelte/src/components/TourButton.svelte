60646528410ab9e5e7c672cccb5d9e5a82ff2f9b
hchiam <hchiam@gmail.com>
2020-05-12 21:21:50 -0400

    got shepherd.js mostly working now
    https://github.com/shipshapecode/shepherd/issues/948#issuecomment-626666796

--- svelte/src/components/TourButton.svelte
+++ svelte/src/components/TourButton.svelte
@@ -1,16 +1,23 @@
 <script>
-  // import { tour, addListeners } from "../helpers/tour.js";
+  import { onMount } from "svelte";
 
   let showButton = false;
   setTimeout(() => {
     showButton = true;
   }, 3000);
 
-  const runTour = () => {
-    tour.start();
-    addListeners();
-    document.body.classList.add("shepherd-active");
-  };
+  let tour, addListeners, runTour;
+
+  onMount(async () => {
+    // https://sapper.svelte.dev/docs#Making_a_component_SSR_compatible
+    const { tour, addListeners } = await import("../helpers/tour.js");
+
+    runTour = () => {
+      tour.start();
+      addListeners();
+      document.body.classList.add("shepherd-active");
+    };
+  });
 </script>
 
 <style>
@@ -20,5 +27,11 @@
 </style>
 
 {#if showButton}
-  <button id="tour-button" on:click={runTour}>What is this?</button>
+  <button
+    id="tour-button"
+    on:click={() => {
+      runTour();
+    }}>
+    What is this?
+  </button>
 {/if}
